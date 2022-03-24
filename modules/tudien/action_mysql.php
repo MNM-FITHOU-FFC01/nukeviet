9aeb4980379196355d0f61241a65d16ad1c54253
VanThe812 <vvthe8102002@gmail.com>
2022-03-24 10:20:03 +0700

    Cập nhật action mysql trong module tudien - lancuoi

--- modules/tudien/action_mysql.php
+++ modules/tudien/action_mysql.php
@@ -15,19 +15,23 @@ if (!defined('NV_IS_FILE_MODULES')) {
 
 $sql_drop_module = [];
 
-$sql_drop_module[] = 'DROP TABLE IF EXISTS ' . $db_config['prefix'] . '_' . $lang . '_' . $module_data . '_tudien';
 $sql_drop_module[] = 'DROP TABLE IF EXISTS ' . $db_config['prefix'] . '_' . $lang . '_' . $module_data . '_tu';
+$sql_drop_module[] = 'DROP TABLE IF EXISTS ' . $db_config['prefix'] . '_' . $lang . '_' . $module_data . '_loaitudien';
 
 $sql_create_module = $sql_drop_module;
 
-$sql_create_module[] = 'CREATE TABLE ' . $db_config['prefix'] . '_' . $lang . '_' . $module_data . "_loaitudien (
- id int(11) unsigned NOT NULL AUTO_INCREMENT,
- tenloaitd varchar(250) NOT NULL,
-) ENGINE=MyISAM";
+$sql_create_module[] = 'CREATE TABLE ' . $db_config['prefix'] . '_' . $lang . '_' . $module_data . "_loaitudien ( 
+    `id` INT(11) NOT NULL , 
+    `tenloaitd` VARCHAR(250) NOT NULL , 
+    PRIMARY KEY (`id`)
+    ) ENGINE = InnoDB;";
 
 $sql_create_module[] = 'CREATE TABLE ' . $db_config['prefix'] . '_' . $lang . '_' . $module_data . "_tu (
-    id int(11) unsigned NOT NULL AUTO_INCREMENT,
-    tentu varchar(250) NOT NULL,
-    nghiatu varchar(250) NOT NULL,
-    id_loaitu int(11) NOT NULL,
-   ) ENGINE=MyISAM";
+    `id` INT(11) NOT NULL , 
+    `tentu` VARCHAR(250) NOT NULL , 
+    `nghiatu` VARCHAR(250) NOT NULL , 
+    `id_loaitu` INT(11) NOT NULL , 
+    PRIMARY KEY (`id`)
+    ) ENGINE = InnoDB;";
+
+
