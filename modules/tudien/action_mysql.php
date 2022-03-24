1a6c4f41219261cc0b0ddca82782148ec5817b41
VanThe812 <vvthe8102002@gmail.com>
2022-03-24 09:59:21 +0700

    cập nhập file action mysql của module tudien

--- modules/tudien/action_mysql.php
+++ modules/tudien/action_mysql.php
@@ -9,19 +9,25 @@
  * @see https://github.com/nukeviet The NukeViet CMS GitHub project
  */
 
-if (!defined('NV_IS_FILE_TUDIEN')) {
+if (!defined('NV_IS_FILE_MODULES')) {
     exit('Stop!!!');
 }
 
 $sql_drop_module = [];
 
-// $sql_drop_module[] = 'DROP TABLE IF EXISTS ' . $db_config['prefix'] . '_' . $lang . '_' . $module_data . '_tudien';
+$sql_drop_module[] = 'DROP TABLE IF EXISTS ' . $db_config['prefix'] . '_' . $lang . '_' . $module_data . '_tudien';
+$sql_drop_module[] = 'DROP TABLE IF EXISTS ' . $db_config['prefix'] . '_' . $lang . '_' . $module_data . '_tu';
 
 $sql_create_module = $sql_drop_module;
 
-// $sql_create_module[] = 'CREATE TABLE ' . $db_config['prefix'] . '_' . $lang . '_' . $module_data . "_tudien (
-//  id smallint(5) unsigned NOT NULL AUTO_INCREMENT,
-//  tentd varchar(250) NOT NULL,
-//  kihieu varchar(250) NOT NULL,
-// ) ENGINE=MyISAM";
+$sql_create_module[] = 'CREATE TABLE ' . $db_config['prefix'] . '_' . $lang . '_' . $module_data . "_loaitudien (
+ id int(11) unsigned NOT NULL AUTO_INCREMENT,
+ tenloaitd varchar(250) NOT NULL,
+) ENGINE=MyISAM";
 
+$sql_create_module[] = 'CREATE TABLE ' . $db_config['prefix'] . '_' . $lang . '_' . $module_data . "_tu (
+    id int(11) unsigned NOT NULL AUTO_INCREMENT,
+    tentu varchar(250) NOT NULL,
+    nghiatu varchar(250) NOT NULL,
+    id_loaitu int(11) NOT NULL,
+   ) ENGINE=MyISAM";
